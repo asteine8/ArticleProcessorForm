129300cb582417802412da95991829d8bdc958f0
Phillia <37705707+psteine3@users.noreply.github.com>
2018-11-07 14:24:37 -0800

    Initial commit

---
+++ ArticleProcesserForm/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace ArticleProcesserForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputDirectoryTextBox = new System.Windows.Forms.TextBox();
+            this.OutputDirectoryTextBox = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.InputDirectorySearchButton = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.OutputDirectorySearchButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.RunButton = new System.Windows.Forms.Button();
+            this.CategoryListBox = new System.Windows.Forms.ListBox();
+            this.KeyTermsListBox = new System.Windows.Forms.ListBox();
+            this.OutputListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // InputDirectoryTextBox
+            // 
+            this.InputDirectoryTextBox.Location = new System.Drawing.Point(12, 35);
+            this.InputDirectoryTextBox.Name = "InputDirectoryTextBox";
+            this.InputDirectoryTextBox.Size = new System.Drawing.Size(300, 20);
+            this.InputDirectoryTextBox.TabIndex = 0;
+            // 
+            // OutputDirectoryTextBox
+            // 
+            this.OutputDirectoryTextBox.Location = new System.Drawing.Point(13, 83);
+            this.OutputDirectoryTextBox.Name = "OutputDirectoryTextBox";
+            this.OutputDirectoryTextBox.Size = new System.Drawing.Size(300, 20);
+            this.OutputDirectoryTextBox.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // InputDirectorySearchButton
+            // 
+            this.InputDirectorySearchButton.Location = new System.Drawing.Point(318, 33);
+            this.InputDirectorySearchButton.Name = "InputDirectorySearchButton";
+            this.InputDirectorySearchButton.Size = new System.Drawing.Size(154, 23);
+            this.InputDirectorySearchButton.TabIndex = 3;
+            this.InputDirectorySearchButton.Text = "Choose File";
+            this.InputDirectorySearchButton.UseVisualStyleBackColor = true;
+            this.InputDirectorySearchButton.Click += new System.EventHandler(this.InputDirectorySearchButton_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // OutputDirectorySearchButton
+            // 
+            this.OutputDirectorySearchButton.Location = new System.Drawing.Point(319, 81);
+            this.OutputDirectorySearchButton.Name = "OutputDirectorySearchButton";
+            this.OutputDirectorySearchButton.Size = new System.Drawing.Size(153, 23);
+            this.OutputDirectorySearchButton.TabIndex = 4;
+            this.OutputDirectorySearchButton.Text = "Choose Directory";
+            this.OutputDirectorySearchButton.UseVisualStyleBackColor = true;
+            this.OutputDirectorySearchButton.Click += new System.EventHandler(this.OutputDirectorySearchButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Source File";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Output Directory";
+            // 
+            // RunButton
+            // 
+            this.RunButton.Location = new System.Drawing.Point(13, 123);
+            this.RunButton.Name = "RunButton";
+            this.RunButton.Size = new System.Drawing.Size(75, 23);
+            this.RunButton.TabIndex = 7;
+            this.RunButton.Text = "Run Search";
+            this.RunButton.UseVisualStyleBackColor = true;
+            this.RunButton.Click += new System.EventHandler(this.RunButton_Click);
+            // 
+            // CategoryListBox
+            // 
+            this.CategoryListBox.FormattingEnabled = true;
+            this.CategoryListBox.Location = new System.Drawing.Point(13, 153);
+            this.CategoryListBox.Name = "CategoryListBox";
+            this.CategoryListBox.Size = new System.Drawing.Size(220, 199);
+            this.CategoryListBox.TabIndex = 8;
+            // 
+            // KeyTermsListBox
+            // 
+            this.KeyTermsListBox.FormattingEnabled = true;
+            this.KeyTermsListBox.Location = new System.Drawing.Point(252, 153);
+            this.KeyTermsListBox.Name = "KeyTermsListBox";
+            this.KeyTermsListBox.Size = new System.Drawing.Size(220, 199);
+            this.KeyTermsListBox.TabIndex = 9;
+            // 
+            // OutputListBox
+            // 
+            this.OutputListBox.BackColor = System.Drawing.SystemColors.Menu;
+            this.OutputListBox.ForeColor = System.Drawing.Color.Black;
+            this.OutputListBox.FormattingEnabled = true;
+            this.OutputListBox.HorizontalScrollbar = true;
+            this.OutputListBox.Location = new System.Drawing.Point(487, 10);
+            this.OutputListBox.Name = "OutputListBox";
+            this.OutputListBox.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.OutputListBox.Size = new System.Drawing.Size(485, 342);
+            this.OutputListBox.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 361);
+            this.Controls.Add(this.OutputListBox);
+            this.Controls.Add(this.KeyTermsListBox);
+            this.Controls.Add(this.CategoryListBox);
+            this.Controls.Add(this.RunButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.OutputDirectorySearchButton);
+            this.Controls.Add(this.InputDirectorySearchButton);
+            this.Controls.Add(this.OutputDirectoryTextBox);
+            this.Controls.Add(this.InputDirectoryTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox InputDirectoryTextBox;
+        private System.Windows.Forms.TextBox OutputDirectoryTextBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button InputDirectorySearchButton;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button OutputDirectorySearchButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button RunButton;
+        private System.Windows.Forms.ListBox CategoryListBox;
+        private System.Windows.Forms.ListBox KeyTermsListBox;
+        private System.Windows.Forms.ListBox OutputListBox;
+    }
+}
+
---
+++ ArticleProcesserForm/Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Diagnostics;
+using System.Threading;
+using Xceed.Words.NET; // Allows 4 handling of word files
+
+public struct Article { // For storing articles in raw text form
+    public string Header;
+    public string Body;
+
+    public List<string> HeaderWords;
+    public List<string> BodyWords;
+
+    public string Source;
+    public string Date;
+
+    public int categoryIndex;
+
+    public Article(string head, string body) {
+        Header = head;
+        Body = body;
+
+        HeaderWords = new List<string>();
+        BodyWords = new List<string>();
+
+        Source = "";
+        Date = "";
+
+        categoryIndex = -1; // Unassigned to category
+    }
+}
+
+namespace ArticleProcesserForm {
+    public partial class Form1 : Form {
+
+        public string InputFilePath = "";
+        public string OutputDirectoryPath = "";
+
+        public List<Article> Articles = new List<Article>();
+
+        public Form1() {
+            InitializeComponent();
+            openFileDialog1.Filter = "Word Document Files (*.docx)|*.docx|All files (*.*)|*.*";
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e) {
+            
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e) {
+
+        }
+
+        private void RunButton_Click(object sender, EventArgs e) {
+            
+            
+        }
+
+        private void InputDirectorySearchButton_Click(object sender, EventArgs e) {
+            // Opens a dialog to get the file location of the target file
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+                // Console.WriteLine(openFileDialog1.FileName);
+                InputFilePath = openFileDialog1.FileName;
+                InputDirectoryTextBox.Text = InputFilePath;
+
+
+                TextProcessing txt = new TextProcessing();
+
+                //txt.GetStringsFromFile(InputFilePath);
+
+                List<string> DocumentLines = txt.GetLinesFromFile(InputFilePath);
+                //string[] lines = documentText.Split('\n');
+
+                this.Articles = txt.GetArticlesFromLines(DocumentLines);
+
+                this.Articles = txt.ProcessWordsFromArticleText(this.Articles);
+
+                OutputListBox.Items.Clear(); // Clear listbox before adding stuff
+
+                for (int i = 0; i < Articles.Count; i++) {
+                    OutputListBox.Items.Add(Articles[i].Header);
+                    //Console.WriteLine(Articles[i].Body);
+                }
+            }
+        }
+
+        private void OutputDirectorySearchButton_Click(object sender, EventArgs e) {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK) {
+                // Console.WriteLine(folderBrowserDialog1.SelectedPath);
+                OutputDirectoryPath = folderBrowserDialog1.SelectedPath;
+                OutputDirectoryTextBox.Text = OutputDirectoryPath;
+            }
+        }
+    }
+}
+
+public class TextProcessing {
+
+    private char[] PunctuationCharacters = { '.', '|', ',', '\n', '\r', ';', '"', '(', ')', '[', ']', '?', '/', '\\', '!', '#', '=', '-', '—', ':', '&', '_', '~'};
+
+    public List<string> GetLinesFromFile(string filePath) {
+        List<string> lines = new List<string>();
+
+        //Stopwatch stopwatch = new Stopwatch();
+        //stopwatch.Start();
+
+        //DocX doc = DocX.Load(filePath);
+        //string documentText = doc.Text;
+        //Console.WriteLine(documentText);
+
+        DocX doc = DocX.Load(filePath);
+        System.Collections.ObjectModel.ReadOnlyCollection<Paragraph> paragraphs = doc.Paragraphs;
+
+        for (int i = 0; i < paragraphs.Count; i++) {
+            lines.Add(paragraphs[i].Text);
+
+            //Console.WriteLine("Loading paragraph " + (i+1).ToString() + " of " + paragraphs.Count.ToString());
+        }
+
+        //stopwatch.Stop();
+        //Console.WriteLine("Time elapsed: " + stopwatch.ElapsedMilliseconds.ToString() + "ms");
+
+        return lines;
+    }
+
+    public List<Article> GetArticlesFromLines(List<string> lines) {
+        List<Article> articles = new List<Article>();
+
+        lines = RemoveAdjacentBlankLinesAndTrim(lines);
+
+        string HeadString = "";
+        string BodyString = "";
+        int linesSinceDelimeter = 0;
+        for (int i = 1; i < lines.Count-1; i++) { // Make list of articles delimiting by empty lines
+
+
+            if (string.IsNullOrEmpty(lines[i])) {
+                // Create new article
+                articles.Add(new Article(HeadString, "|" + BodyString + "|")); // Add a new article
+                HeadString = "";
+                BodyString = "";
+                linesSinceDelimeter = 0; // Reset counter
+                continue;
+            }
+
+            if (linesSinceDelimeter == 0) { // This is a header line
+                HeadString = lines[i];
+            }
+            else { // This is a body line
+                BodyString += lines[i] + " "; // Add a space for neatness
+            }
+            linesSinceDelimeter++;
+        }
+        return articles;
+    }
+
+    // Function for converting multiple adjecent empty newlines from List<string> to prep for processing into individual articles
+    private List<string> RemoveAdjacentBlankLinesAndTrim(List<string> lines) {
+        List<string> newLines = new List<string>();
+        for (int i = 0; i < lines.Count-1; i++) {
+            // Search for the last delimeter index
+            if (string.IsNullOrEmpty(lines[i].Trim()) && !string.IsNullOrEmpty(lines[i + 1].Trim())) {
+                newLines.Add(lines[i].Trim());
+            }
+            else if (lines[i].Length != 0) {
+                newLines.Add(lines[i].Trim());
+            }
+        }
+        return newLines;
+    }
+
+    public List<Article> ProcessWordsFromArticleText(List<Article> articles) {
+
+        for (int i = 0; i < articles.Count; i ++) { // Iterate through all articles
+            Article tempArticle = articles[i];
+
+            string dateString = GetTextBetweenStrings(tempArticle.Body, "Date:", "|");
+            string sourceString = GetTextBetweenStrings(tempArticle.Body + "|", "Source:", "|");
+            tempArticle.Date = dateString;
+            tempArticle.Source = sourceString;
+
+            string bodyString = tempArticle.Body;
+            bodyString = ReplaceCharactersWithSpace(bodyString, PunctuationCharacters); // Replace punctuation with spaces
+            bodyString = RemoveAdjacentDuplicatesOfChar(bodyString, ' '); // Remove duplicate adjecent spaces
+            // Parse string list from string delimiting by spaces
+            tempArticle.BodyWords = new List<string>(bodyString.Trim().Split(' '));
+
+            string headerString = tempArticle.Header;
+            headerString = ReplaceCharactersWithSpace(headerString, PunctuationCharacters);
+            headerString = RemoveAdjacentDuplicatesOfChar(headerString, ' ');
+            tempArticle.HeaderWords = new List<string>(headerString.Trim().Split(' '));
+
+            //for (int j = 0; j < bodyWords.Count; j++) {
+            //    Console.Write("\"" + bodyWords[j] + "\"");
+            //}
+            //Console.Write("\n");
+
+            articles[i] = tempArticle;
+        }
+
+        return articles;
+    }
+
+    private string GetTextBetweenStrings(string text, string startDel, string endDel) {
+        string tempText = text;
+
+        // Change all case to lower as to ignore case
+        text = text.ToLower();
+        startDel = startDel.ToLower();
+        endDel = endDel.ToLower();
+
+        int startIndex = text.IndexOf(startDel);
+        if (startIndex == -1) { // No instances found, return empty string
+            return "";
+        }
+        int endIndex = text.IndexOf(endDel, startIndex); // start @ start index
+        if (endIndex <= startIndex) { // No instances found, return empty string
+            return "";
+        }
+        // Delimeters are found, we are go for stripping
+
+        startIndex += startDel.Length; // Add length to start index so we don't include the start delimeter
+
+        return tempText.Substring(startIndex, endIndex - startIndex - 1).Trim();
+    }
+
+    private string ReplaceCharactersWithSpace(string RString, char[] Chars2Remove) {
+        for (int i = 0; i < Chars2Remove.Length; i++) {
+            RString = RString.Replace(Chars2Remove[i], ' ');
+        }
+        return RString;
+    }
+    
+    private string RemoveAdjacentDuplicatesOfChar(string RString, char searchChar) {
+        for (int i = RString.Length - 2; i >= 0; i--) {
+            if (RString[i] == RString[i+1] && RString[i] == searchChar) {
+                RString = RString.Remove(i + 1, 1); // Remove the character @ i + 1 and that character only
+            }
+        }
+        return RString;
+    }
+}
